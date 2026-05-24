70f0eb00669c5bd3af153d1f369aff5a38ecd74a
Open_MyTrust_profile <clementbtc2023@gmail.com>
2026-05-24 22:30:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -69,51 +69,7 @@ loadTransactions();
 );
 
 const themeToggle = document.getElementById("themeToggle");
-const sun = document.getElementById("iconSun");
-const moon = document.getElementById("iconMoon");
-
-function setTheme(mode){
-
-if(mode === "light"){
-document.body.classList.add("lightMode");
-sun.style.display = "none";
-moon.style.display = "block";
-}else{
-document.body.classList.remove("lightMode");
-sun.style.display = "block";
-moon.style.display = "none";
-}
-
-localStorage.setItem("theme", mode);
-}
 
 themeToggle.onclick = () => {
-const isLight = document.body.classList.contains("lightMode");
-setTheme(isLight ? "dark" : "light");
+document.body.classList.toggle("lightMode");
 };
-
-setTheme(localStorage.getItem("theme") || "dark");
-themeToggle.addEventListener("click", function(e){
-
-const ripple = document.createElement("span");
-
-const rect = this.getBoundingClientRect();
-
-const size = Math.max(rect.width, rect.height);
-
-const x = e.clientX - rect.left - size / 2;
-const y = e.clientY - rect.top - size / 2;
-
-ripple.style.width = ripple.style.height = size + "px";
-ripple.style.left = x + "px";
-ripple.style.top = y + "px";
-
-ripple.classList.add("ripple");
-
-this.appendChild(ripple);
-
-setTimeout(()=>{
-ripple.remove();
-},600);
-
-});
