68267b7a659722185a0a6729854bf046eadfe5c5
Open_MyTrust_profile <clementbtc2023@gmail.com>
2026-05-24 20:28:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -93,3 +93,27 @@ setTheme(isLight ? "dark" : "light");
 };
 
 setTheme(localStorage.getItem("theme") || "dark");
+themeToggle.addEventListener("click", function(e){
+
+const ripple = document.createElement("span");
+
+const rect = this.getBoundingClientRect();
+
+const size = Math.max(rect.width, rect.height);
+
+const x = e.clientX - rect.left - size / 2;
+const y = e.clientY - rect.top - size / 2;
+
+ripple.style.width = ripple.style.height = size + "px";
+ripple.style.left = x + "px";
+ripple.style.top = y + "px";
+
+ripple.classList.add("ripple");
+
+this.appendChild(ripple);
+
+setTimeout(()=>{
+ripple.remove();
+},600);
+
+});
